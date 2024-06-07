7b32426dbbff03c038485d433707ba6df1ec6c0d
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-06-07 19:10:59 +0330

    Feat(Component):create options year create options year with Date and forward on select tag in html

---
+++ components/CreateOptionsYear/yearOptionsCar.js
@@ -0,0 +1,16 @@
+function createOptionYear() {
+    let select = document.querySelector(".selectOptionCarYear .selectComponent");
+  
+    // name
+    let year = new Date();
+    let nowYear = year.getUTCFullYear();
+    let options = [];
+  
+    for (let j = nowYear; j >= nowYear - 20; j--) {
+      const newOption = document.createElement("option");
+      newOption.textContent = j;
+      select.append(newOption);
+      options.push(j);
+    }
+  }
+  export default createOptionYear;
