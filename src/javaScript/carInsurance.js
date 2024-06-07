822bf8339bffa5f17ef581a81a0ce764c17ba725
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-06-07 19:21:55 +0330

    Feat(javaScript):main file parent file for all js file

---
+++ src/javaScript/carInsurance.js
@@ -0,0 +1,36 @@
+import domGenerator from "dom-generator";
+import "/styles/index.scss";
+
+import createOptionsCarNames from "../../components/createOptionsName/optionsName";
+import createOptionYear from "../../components/CreateOptionsYear/yearOptionsCar";
+import formInsurance from "../../components/formGenerator/formInsurance";
+
+function carInsurance() {
+  let insurance = domGenerator({
+    tag: "div",
+    attributes: { id: "insuranceBackground" },
+    children: [
+      {
+        tag: "section",
+        attributes: { id: "insuranceSection" },
+        children: [
+          {
+            tag: "h1",
+            attributes: { id: "formInsuranceTitle" },
+            properties: { textContent: "form generator" },
+          },
+          {
+            tag: formInsurance(),
+          },
+        ],
+      },
+    ],
+  });
+  document.addEventListener("DOMContentLoaded", renderCars);
+  function renderCars() {
+    createOptionYear();
+    createOptionsCarNames();
+  }
+  document.body.append(insurance);
+}
+export default carInsurance;
